c3dd3d1c28f30b873e3e1e5a529660bbd029bb27
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2018-01-20 04:25:26 -0800

    Define constraint variable separately, pass var in function

--- sbhacks/game.js
+++ sbhacks/game.js
@@ -2,8 +2,12 @@ var video = document.querySelector("#videoElement");
  
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
  
-if (navigator.getUserMedia) {       
-    navigator.getUserMedia({video: true, facingMode: "environment"}, handleVideo, videoError);
+if (navigator.getUserMedia) {
+	var constraints = {
+		video: true,
+		facingMode: "environment"
+	};
+    navigator.getUserMedia(constraints, handleVideo, videoError);
 }
  
 function handleVideo(stream) {
