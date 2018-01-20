4a3998a1d27f71d712ab50e0e98cad990e4baee5
Andrew Lu <34492835+andrewhlu@users.noreply.github.com>
2018-01-20 09:46:54 -0800

    Firebase Login Button

--- sbhacks/game.js
+++ sbhacks/game.js
@@ -32,8 +32,45 @@ function captureImage() {
     console.log(imagelink);
 }
 
-//Firebase Auth
-FriendlyChat.prototype.initFirebase = function() {
+//Firebase Initialization
+function ScavengerHunt() {
+  this.checkSetup();
+
+  // Shortcuts to DOM Elements.
+  // this.messageList = document.getElementById('messages');
+  // this.messageForm = document.getElementById('message-form');
+  // this.messageInput = document.getElementById('message');
+  // this.submitButton = document.getElementById('submit');
+  // this.submitImageButton = document.getElementById('submitImage');
+  // this.imageForm = document.getElementById('image-form');
+  // this.mediaCapture = document.getElementById('mediaCapture');
+  // this.userPic = document.getElementById('user-pic');
+  // this.userName = document.getElementById('user-name');
+   this.signInButton = document.getElementById('sign-in');
+  // this.signOutButton = document.getElementById('sign-out');
+  // this.signInSnackbar = document.getElementById('must-signin-snackbar');
+
+  // Saves message on form submit.
+  //this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+  //this.signOutButton.addEventListener('click', this.signOut.bind(this));
+  this.signInButton.addEventListener('click', this.signIn.bind(this));
+
+  // Toggle for the button.
+  // var buttonTogglingHandler = this.toggleButton.bind(this);
+  // this.messageInput.addEventListener('keyup', buttonTogglingHandler);
+  // this.messageInput.addEventListener('change', buttonTogglingHandler);
+
+  // Events for image upload.
+  // this.submitImageButton.addEventListener('click', function(e) {
+  //   e.preventDefault();
+  //   this.mediaCapture.click();
+  // }.bind(this));
+  // this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+  this.initFirebase();
+}
+
+ScavengerHunt.prototype.initFirebase = function() {
   // TODO(DEVELOPER): Initialize Firebase.
   //Shortcuts to Firebase SDK features
   this.auth = firebase.auth();
@@ -43,4 +80,8 @@ FriendlyChat.prototype.initFirebase = function() {
   this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
-this.initFirebase();
+ScavengerHunt.prototype.signIn = function() {
+  // TODO(DEVELOPER): Sign in Firebase with credential from the Google user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
